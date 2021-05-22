5d2bb5687163ffbb6c867a0690506b50bdee1309
PedroSousa97 <henriquesantos293@gmail.com>
2021-05-22 16:50:35 +0100

    Delete Property with existing name validation

--- api/controller/controller.js
+++ api/controller/controller.js
@@ -7,7 +7,7 @@ const DBPASS = process.env.DBPASSWORD;
 const pool = mariadb.createPool({host: '127.0.0.1',port:'3306', user: 'root', database:'techchallengedb', password: DBPASS, connectionLimit: 5});
 
 
-//add a new Property in one POST request:
+//Add a new Property in one POST request:
 exports.createProperty = function(req,res){
     propertyName = req.body.name; //property name request parameter
     propertyUnits = req.body.units //array of units request parameter
@@ -23,7 +23,7 @@ exports.createProperty = function(req,res){
                 return res.status(400).json({message:"The chosen property name is already in use"})
             }
         }).catch(err => {
-            console.log(err); //in case of erro, log the error just for debugging purposes
+            console.log(err); //in case of error, log the error just for debugging purposes
             conn.end();
         })
         //In this case instead of a for cycle, I'll use the map function that returns promisses. A for loop wouldn't work because the code after it would execute first
@@ -34,7 +34,7 @@ exports.createProperty = function(req,res){
                     UnitValidator = UnitValidator + parseInt(rows[0].matches) //If the name matches one of the lookup table, add +1 to the validator
                     resolve(UnitValidator) //Add the updated value to the resolve array
                 }).catch(err => {
-                    console.log(err); //in case of erro, log the error just for debugging purposes
+                    console.log(err); //in case of error, log the error just for debugging purposes
                     conn.end();
                 })
             })})).then(UnitValidator=>{ //After all promisses are resolved, continue code execution 
@@ -55,7 +55,7 @@ exports.createProperty = function(req,res){
                     conn.end();
                     return res.status(201).json({message:"Property created successfully"}) //At this point, property should be created, Created status message is sent
                 }).catch(err => {
-                    console.log(err); //in case of erro, log the error just for debugging purposes
+                    console.log(err); //in case of error, log the error just for debugging purposes
                     conn.end();
                   })
             })
@@ -63,7 +63,48 @@ exports.createProperty = function(req,res){
       }).catch(err =>{  //Catch block to detect and catch DB connection issues
         conn.end();
           return res.status(500).json({
-            code: "0",
             message: "Couldn't connect to the Database!",
           })})
+}
+
+
+
+//delete a property functionality
+exports.deleteProperty = function(req,res){
+    propertyName = req.body.name; //property name request parameter
+    id = 0;
+
+    pool.getConnection()
+        .then(conn => {
+            //First of all, let's check if the received property name exist
+            conn.query("SELECT idProperties AS id FROM properties WHERE properties.Name Like '"+propertyName+"'")
+            .then((rows) => {
+                if(!rows[0]){  //if now results were return, that means that the received property name doesn't exist
+                    conn.end(); //In this case send a bad request error message                     
+                    return res.status(400).json({message:"The chosen property name doesn't exist"})
+                }
+                id= parseInt(rows[0].id) //Save the id for further queries, and try the deletes, first the child table then the parent one
+                conn.query("DELETE FROM propertyrooms WHERE propertyrooms.Properties_idProperties = '"+id+"'")
+                .then((rows) => {
+                    conn.query("DELETE FROM properties WHERE idProperties = '"+id+"'")
+                    .then((rows) => {
+                        conn.end();
+                        return res.status(202).json({message:"Property deleted successfully"}) //At this point, property should be deleted, Accepted status message is sent
+                    }).catch(err => {
+                        console.log(err); //in case of error, log the error just for debugging purposes
+                        conn.end();
+                    })
+                }).catch(err => {
+                    console.log(err); //in case of error, log the error just for debugging purposes
+                    conn.end();
+                })
+            }).catch(err => {
+                console.log(err); //in case of error, log the error just for debugging purposes
+                conn.end();
+            })
+        }).catch(err =>{  //Catch block to detect and catch DB connection issues
+        conn.end();
+          return res.status(500).json({
+            message: "Couldn't connect to the Database!",
+        })})
 }
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,7 +3,8 @@ const controller = require("../controller/controller")
 const routes = (app)=>{
     //Routes will be defined here
     app.route('/properties')
-      .post(controller.createProperty)
+      .post(controller.createProperty) //Create Property logic is on the controller
+      .delete(controller.deleteProperty) //Delete Property logic is on the controller
 }
 
 module.exports = routes;
